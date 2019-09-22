a5a43aa4195511986690c2157112fc3de8d06c99
Peter Ward <peteraward@gmail.com>
2019-09-22 15:59:01 +1000

    Release new version.

---
+++ README.md
@@ -0,0 +1,3 @@
+This library is an implementation of the [PJLink](https://pjlink.jbmia.or.jp/)
+protocol. There is both an API and a command-line tool: for usage information,
+see [this blog post](https://blog.flowblok.id.au/2012-11/controlling-projectors-with-pjlink.html).
--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='pjlink',
-    version='1.0',
+    version='1.1',
     author='Peter Ward',
     author_email='peteraward@gmail.com',
     url='http://hg.flowblok.id.au/pjlink',
     description='PJLink is a standard for controlling data projectors.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
